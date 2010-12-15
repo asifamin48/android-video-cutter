877465e17bc85e679dc57f0c67015aa19bc7bf09
kiranpratap <kiranpratap@gmail.com>
2010-12-15 20:13:31 +0530

    fixed compilation/IDE/Linking errors

--- jni/include/net_avc_video_cutter_natives_Natives.h
+++ jni/include/net_avc_video_cutter_natives_Natives.h
@@ -2,8 +2,8 @@
 #include <jni.h>
 /* Header for class com_ruk_tutorials_natives_Natives */
 
-#ifndef _Included_com_ruk_tutorials_natives_Natives
-#define _Included_com_ruk_tutorials_natives_Natives
+#ifndef _Included_net_avc_video_cutter_natives_Natives
+#define _Included_net_avc_video_cutter_natives_Natives
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,7 +12,7 @@ extern "C" {
  * Method:    takePics
  * Signature: ()I
  */
-JNIEXPORT jint JNICALL Java_com_ruk_tutorials_natives_Natives_takePics
+JNIEXPORT jint JNICALL Java_net_avc_video_cutter_natives_Natives_takePics
   (JNIEnv *, jclass);
 
 #ifdef __cplusplus
